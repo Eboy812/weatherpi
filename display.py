52e68507a854b4d0d0e4a7ce92fb02aad6107288
Eboy812 <eboy.ford@gmail.com>
2019-04-11 14:31:37 -0500

    Update

--- display.py
+++ display.py
@@ -42,7 +42,7 @@ def lan_ip():
 def report():
     data = bme280.sample(bus, address, calibration_params)
     API_ENDPOINT = 'http://ford.shelms.io/api/'
-    r = requests.post(API_ENPOINT,
+    r = requests.post(API_ENDPOINT,
 	data ={'celsius': data.temperature})
     print(r.text) 
 
